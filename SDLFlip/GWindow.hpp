39bc560183d4e4b1ff3b208e4cba7e1867584b16
pupungeggang2 <kyongkyong2892@gmail.com>
2025-02-03 23:39:54 +0900

    Homebrew builds

--- SDLFlip/GWindow.hpp
+++ SDLFlip/GWindow.hpp
@@ -4,15 +4,9 @@
 #include <cstdlib>
 #include <string>
 #include <glad/glad.h>
-#ifdef __APPLE__
-#include <SDL.h>
-#include <SDL_image.h>
-#include <SDL_ttf.h>
-#else
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 #include <SDL2/SDL_ttf.h>
-#endif
 
 void SWindowInit();
 void SWindowLoop();

a120f911b17dff92df747ede77a2bfb64dd17f2a
pupungeggang2 <kyongkyong2892@gmail.com>
2025-02-04 23:57:10 +0900

    Temp

--- SDLTriangle/main.cpp
+++ SDLTriangle/main.cpp
@@ -75,15 +75,7 @@ int main(int argc, char* argv[]) {
     SDL_Init(SDL_INIT_EVERYTHING);
     window = SDL_CreateWindow("Triangle Example", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_OPENGL);
     context = SDL_GL_CreateContext(window);
-
-    #ifndef __APPLE__
-    gladLoadGLES2Loader(SDL_GL_GetProcAddress);
-    #else
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_COMPATIBILITY);
-    gladLoadGL();
-    #endif
+    gladLoadGLLoader(SDL_GL_GetProcAddress);
 
     GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
     glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
